2260225caf6b191e37710a84e567e8a3655fa10d
Colin Barry <colinpbarry@Colins-MacBook-Air-2.local>
2016-10-16 18:24:01 -0400

    Closes #6

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -59,6 +59,12 @@ module.exports = {
         test: /\.(jpe?g|gif|png|svg)$/i,
         loader: 'url-loader?limit=10000',
       }, {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.(ttf|otf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      }, {
         test: /\.json$/,
         loader: 'json-loader',
       },
